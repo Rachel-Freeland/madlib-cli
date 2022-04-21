67a871cd0429a70f58886f680dbaa5f8f3c73e22
Rachel Freeland <rachel.freeland1@gmail.com>
2022-04-21 11:41:31 -0500

    Added assets with sample template and a function

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,9 +1,12 @@
-file_name = "assets/dark_and_stormy_night_template.txt"
-def read_template(file_name):
-  try:
-    with open(file_name, 'r') as f:
-      text = f.read()
-  except:
-    pass
-  finally:
-    pass
+path = 'assets/dark_and_stormy_night_template.txt'
+def read_template(path):
+  with open(path, 'r') as f:
+    text = f.read()
+    return text
+
+def parse_template(path):
+  pass
+
+def merge_parts():
+  pass
+

cd454f921d2cf80703b0f021787660d692c41cb0
Rachel Freeland <rachel.freeland1@gmail.com>
2022-04-21 11:07:46 -0500

    Not solved yet

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,6 +1,8 @@
-def read_template():
+file_name = "assets/dark_and_stormy_night_template.txt"
+def read_template(file_name):
   try:
-    pass
+    with open(file_name, 'r') as f:
+      text = f.read()
   except:
     pass
   finally:
